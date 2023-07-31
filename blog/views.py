82f26de32ae3e83e8f5ec24030e6a58d71c02e55
ritika <ritika.tiwari@otocapital.in>
2023-07-31 13:27:41 +0530

    added comments../

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,10 @@
 from django.core.mail import send_mail
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage
+from django.views.decorators.http import require_POST
 
-from .forms import EmailPostForm
-from .models import Post
+from .forms import EmailPostForm, CommentForm
+from .models import Post, Comment
 
 
 def post_list(request):
@@ -24,7 +25,11 @@ def post_detail(request, year, month, day, post):
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
-    return render(request, 'detail.html', {'post': post})
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
+    return render(request, 'detail.html', {'post': post,
+                                                                'comments': comments,
+                                           'form': form})
 
 
 def post_share(request, post_id):
@@ -45,3 +50,19 @@ def post_share(request, post_id):
     return render(request, 'share.html', {'post':post,
                                           'form': form,
                                           'sent': sent})
+
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        # create a comment object without saving it to the database
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+    return render(request, 'comment.html', {'post':post,
+                                                                'form': form,
+                                                                 'comment': comment})
